9c137e89b70ee29b52bf63db605ab50d9f435a62
ztang <ztang@uger-c009.broadinstitute.org>
2024-06-28 16:50:23 +0000

    add docs

---
+++ docs/source/conf.py
@@ -0,0 +1,34 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'CAST'
+copyright = '2024, Zefang Tang; Shuchen Luo; Xiao Wang'
+author = 'Zefang Tang; Shuchen Luo; Xiao Wang'
+release = 'Latest'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'nbsphinx',
+    'sphinx_rtd_theme',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+nbsphinx_allow_errors = True  # Continue through errors
+nbsphinx_execute = 'never'   # directly transfer initial ipynb
---
+++ docs/source/index.rst
@@ -0,0 +1,28 @@
+.. CAST documentation master file, created by
+   sphinx-quickstart on Fri Jun 28 16:03:31 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to CAST's documentation!
+================================
+
+**CAST** is a Python library for physically aligning different spatial transcriptome regardless of technologies, magnification, individual variation, and experimental batch effects.
+CAST is composed of three modules: CAST Mark, CAST Stack, and CAST Projection.
+
+`CAST Github <https://github.com/wanglab-broad/CAST>`_
+
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Tutorials:
+
+   notebooks/demo1_CAST_mark.ipynb
+   notebooks/demo2_CAST_Stack_Align_S4_to_S1.ipynb
+   notebooks/demo3_CAST_project.ipynb
+   notebooks/demo4_STARmap_to_MERFISH.ipynb
+
+.. image:: /_static/Diagram.png
+   :alt: Diagram of CAST
+   :align: center
+   :width: 100%
+
